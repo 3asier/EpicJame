ac66dfc8b17daff4788860e648573364492057d6
Thomas Grantley-Davies <tgrantle@myune.edu.au>
2019-10-21 13:38:47 +1100

    Added main.c and compiled into main.exe

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * Created by,
+ *  - Ton3l & EpicJame
+ * 
+ */
+
+#include <iostream>
+
+bool running = false;
+
+int main() {
+    running = true;
+    while (running) {
+        std::cout << "Running" << std::endl;
+    }
+}
